82b6458035607bde40f14f822119483ada2b24e6
feiy <feiy@nvidia.com>
2023-12-27 11:30:03 +0800

    Fix/refine

--- json.hpp
+++ json.hpp
@@ -10,467 +10,401 @@
 #include <cstring>
 #include <cstdio>
 
-enum class Type
+namespace DataModel
 {
-	Null,
-	Number,
-	Boolean,
-	String,
-	Array,
-	Object
-};
+	template<typename T> class TToken;
 
-class JToken;
-typedef std::shared_ptr<JToken> SPToken;
-
-class JToken
-{
-public:
-	virtual Type type() const = 0;
-	
-	template<typename T>
-	T* To()
+	class Token
 	{
-		return dynamic_cast<T*>(this);
-	}
-
-protected:
-	JToken(){}
-	virtual ~JToken(){}
-};
-
-
-class JNull;
-typedef std::shared_ptr<JNull> SPNull;
-
-class JNull : public JToken
-{
-public:
-	static SPNull New()
-	{
-		return SPNull(new JNull);
-	}
-
-	Type type() const override
-	{
-		return Type::Null;
-	}
-	
-protected:
-	JNull()
-	{
-
-	}
-};
-
-class JNumber;
-typedef std::shared_ptr<JNumber> SPNumber;
-
-class JNumber : public JToken
-{
-public:
-	static SPNumber New(double v = 0.0)
-	{
-		return SPNumber(new JNumber(v));
-	}
-
-	Type type() const override
-	{
-		return Type::Number;
-	}
-	
-	double& value()
-	{
-		return m_value;
-	}
-
-	const double& value() const
-	{
-		return m_value;
-	}
-
-protected:
-	double m_value;
-	JNumber(double v): m_value(v)
-	{
-
-	}
-};
-
-class JBoolean;
-typedef std::shared_ptr<JBoolean> SPBoolean;
-
-class JBoolean : public JToken
-{
-public:
-	static SPBoolean New(bool v = false)
-	{
-		return SPBoolean(new JBoolean(v));
-	}
-
-	Type type() const override
-	{
-		return Type::Boolean;
-	}
-
-	bool& value()
-	{
-		return m_value;
-	}
-
-	const bool& value() const
-	{
-		return m_value;
-	}
-
-protected:
-	bool m_value;
-	JBoolean(bool v) : m_value(v)
-	{
-
-	}
-};
-
-
-class JString;
-typedef std::shared_ptr<JString> SPString;
-
-class JString : public JToken
-{
-public:
-	static SPString New(const char* str = "")
-	{
-		return SPString(new JString(str));
-	}
-
-	Type type() const override
-	{
-		return Type::String;
-	}
-
-	std::string& value()
-	{
-		return m_value;
-	}
-
-	const std::string& value() const
-	{
-		return m_value;
-	}
-
-protected:
-	std::string m_value;
-	JString(const char* str) : m_value(str)
-	{
-		
-	}
-};
-
-class JArray;
-typedef std::shared_ptr<JArray> SPArray;
-
-class JArray : public JToken
-{
-public:
-	static SPArray New(size_t size = 0)
-	{
-		return SPArray(new JArray(size));
-	}
-
-	Type type() const override
-	{
-		return Type::Array;
-	}
-
-	std::vector<SPToken>& value()
-	{
-		return m_value;
-	}
-
-	const std::vector<SPToken>& value() const
-	{
-		return m_value;
-	}
-
-protected:
-	std::vector<SPToken> m_value;
-	JArray(size_t size) : m_value(size)
-	{
-
-	}
-};
-
-class JObject;
-typedef std::shared_ptr<JObject> SPObject;
-
-class JObject : public JToken
-{
-public:
-	static SPObject New()
-	{
-		return SPObject(new JObject);
-	}
-
-	Type type() const override
-	{
-		return Type::Object;
-	}
-
-	std::unordered_map<std::string, SPToken>& value()
-	{
-		return m_value;
-	}
-
-	const std::unordered_map<std::string, SPToken>& value() const
-	{
-		return m_value;
-	}
-
-protected:
-	std::unordered_map<std::string, SPToken> m_value;
-	JObject(){}
-
-};
-
-inline std::string Strigify(SPToken token)
-{
-	std::string str;
-	
-	int indent = 0;
-	std::function<void(SPToken)> func;
-
-	func = [&func, &str, &indent](SPToken token) {
-		if (token->type() == Type::Null)
+	public:
+		template<typename T>
+		T& To()
 		{
-			str += "null";
+			return *dynamic_cast<T*>(this);
 		}
-		else if (token->type() == Type::Number)
-		{
-			char buf[256];
-			sprintf(buf, "%f", token->To<JNumber>()->value());
-			str += buf;
-		}
-		else if (token->type() == Type::Boolean)
-		{
-			str += token->To<JBoolean>()->value() ? "true" : "false";
-		}
-		else if (token->type() == Type::String)
-		{
-			str += std::string("\"") + token->To<JString>()->value() + "\"";
-		}
-		else if (token->type() == Type::Array)
-		{
-			auto& arr = token->To<JArray>()->value();
-			str += "[\n";
-			indent++;
-			for (size_t i = 0; i < arr.size(); i++)
-			{				
-				for (int j = 0; j < indent; j++)
-				{					
-					str += "\t";
-				}				
-				func(arr[i]);
-				if (i < arr.size() - 1)
-				{
-					str += ",\n";
-				}
-			}
-			str += "]\n";
-			indent--;
-		}
-		else if (token->type() == Type::Object)
-		{
-			auto obj = token->To<JObject>()->value();
-			str += "{\n";
-			indent++;
-			auto iter = obj.begin();
-			while (iter != obj.end())
-			{
-				for (int j = 0; j < indent; j++)
-				{
-					str += "\t";
-				}
-				str += std::string("\"") + iter->first + "\": ";
-				func(iter->second);
-				iter++;
-				if (iter != obj.end())
-				{
-					str += ",\n";
-				}
-			}
 
-			str += "}\n";
-			indent--;
+		template<typename T>
+		const T& To() const
+		{
+			return *dynamic_cast<const T*>(this);
+		}
+
+		template<typename T>
+		T& ToValue()
+		{
+			return To<TToken<T>>().value();
+		}
+
+		template<typename T>
+		const T& ToValue() const
+		{
+			return To<TToken<T>>().value();
+		}
+
+	protected:
+		Token(){}
+		virtual ~Token(){}
+	};
+	typedef std::shared_ptr<Token> SPToken;
+
+	template <typename T>
+	class TTokenPtr : public std::shared_ptr<TToken<T>>
+	{
+	public:
+		using std::shared_ptr<TToken<T>>::shared_ptr;
+
+		T& operator*() const
+		{
+			return std::shared_ptr<TToken<T>>::get()->value();
+		}
+
+		T* operator->() const
+		{
+			return &std::shared_ptr<TToken<T>>::get()->value();
 		}
 	};
-	func(token);
-	return str;
+
+	template<typename T>
+	class TToken : public Token
+	{
+	public:
+		static TTokenPtr<T> New(T&& v = T())
+		{
+			return TTokenPtr<T>(new TToken<T>(T(v)));
+		}
+
+		T& value()
+		{
+			return m_value;
+		}
+
+		const T& value() const
+		{
+			return m_value;
+		}
+		
+	protected:
+		T m_value;
+		TToken(T&& v) : m_value(v)
+		{
+
+		}
+	};
+
+	typedef double NumberT;
+	typedef TToken<NumberT> Number;
+	typedef TTokenPtr<NumberT> SPNumber;
+	typedef bool BooleanT;
+	typedef TToken<BooleanT> Boolean;
+	typedef TTokenPtr<BooleanT> SPBoolean;
+	typedef std::string StringT;
+	typedef TToken<StringT> String;
+	typedef TTokenPtr<StringT> SPString;
+	typedef std::vector<SPToken> ArrayT;
+	typedef TToken<ArrayT> Array;
+	typedef TTokenPtr<ArrayT> SPArray;
+	typedef std::unordered_map<std::string, SPToken> ObjectT;
+	typedef TToken<ObjectT> Object;
+	typedef TTokenPtr<ObjectT> SPObject;
+
+	// for extensions, not used
+	class Wrapper : public Token
+	{
+	public:
+		void* ptr()
+		{
+			return m_cptr;
+		}
+
+	protected:
+		void* m_cptr;
+		Wrapper(void* cptr) : m_cptr(cptr)
+		{
+
+		}
+	};
+	typedef std::shared_ptr<Wrapper> SPWrapper;
 }
 
-inline void ltrim(std::string& s) {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-		return !std::isspace(ch);
-		}));
-}
-
-inline void rtrim(std::string& s) {
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-		return !std::isspace(ch);
-		}).base(), s.end());
-}
-
-inline void trim(std::string& s) {
-	rtrim(s);
-	ltrim(s);
-}
-
-inline SPToken Parse(const char* str)
+namespace Json
 {
-	static std::unordered_map<char, char> match_list = {
+	using namespace DataModel;
+
+	inline void ltrim(std::string& s) {
+		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+			return !std::isspace(ch);
+			}));
+	}
+
+	inline void rtrim(std::string& s) {
+		s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+			return !std::isspace(ch);
+			}).base(), s.end());
+	}
+
+	inline void trim(std::string& s) {
+		rtrim(s);
+		ltrim(s);
+	}
+
+	inline std::string Strigify(SPToken token, int indent = 0)
+	{
+		if (token == nullptr)
+		{
+			return "null";
+		}
+
+		{
+			Number* t = dynamic_cast<Number*>(token.get());
+			if (t != nullptr)
+			{
+				char buf[256];
+				sprintf(buf, "%f", t->value());
+				return buf;
+			}
+		}
+
+		{
+			Boolean* t = dynamic_cast<Boolean*>(token.get());
+			if (t != nullptr)
+			{
+				return t->value() ? "true" : "false";
+			}
+		}
+
+		{
+			String* t = dynamic_cast<String*>(token.get());
+			if (t != nullptr)
+			{
+				// TODO: need escape here
+				return std::string("\"") + t->value() + "\"";
+			}
+		}
+
+		{
+			Array* t = dynamic_cast<Array*>(token.get());
+			if (t != nullptr)
+			{
+				const auto& arr = t->value();
+				std::string str = "[\n";
+				indent++;
+				for (size_t i = 0; i < arr.size(); i++)
+				{
+					for (int j = 0; j < indent; j++)
+					{
+						str += "\t";
+					}
+					str += Strigify(arr[i], indent);
+					if (i < arr.size() - 1)
+					{
+						str += ",\n";
+					}
+				}
+				indent--;
+				str += "\n";
+				for (int j = 0; j < indent; j++)
+				{
+					str += "\t";
+				}
+				str += "]";
+				return str;
+			}
+		}
+
+		{
+			Object* t = dynamic_cast<Object*>(token.get());
+			if (t != nullptr)
+			{
+				const auto& obj = t->value();
+				std::string str = "{\n";
+				indent++;
+				auto iter = obj.begin();
+				while (iter != obj.end())
+				{
+					for (int j = 0; j < indent; j++)
+					{
+						str += "\t";
+					}
+					str += std::string("\"") + iter->first + "\": ";
+					str += Strigify(iter->second, indent);					
+					iter++;
+					if (iter != obj.end())
+					{
+						str += ",\n";
+					}
+				}
+				indent--;
+				str += "\n";
+				for (int j = 0; j < indent; j++)
+				{
+					str += "\t";
+				}
+				str += "}";
+				return str;
+			}
+		}
+		return "null";
+	}
+
+	inline SPToken Parse(const char* str)
+	{
+		static std::unordered_map<char, char> match_list = {
 		{'\"', '\"'},
 		{'[', ']'},
 		{'{', '}'},
-	};
+		};
 
-	std::string s = str;
-	trim(s);
+		std::string s = str;
+		trim(s);
 
-	if (s.length() < 1)
-	{
-		return JNull::New();
-	}
 
-	if (s[0] == '\"')
-	{
-		std::string sub = s.substr(1, s.length() - 2);
-		return JString::New(sub.c_str());
-	}
-	
-	if (s[0] == '[')
-	{
-		SPArray arr = JArray::New();
-		std::string inside = s.substr(1, s.length() - 2);
-		std::list<char> stack;
-		int start = 0;
-		int end = start;
-		for (; end <= inside.length(); end++)
+		if (s.length() < 1)
 		{
-			if (end < inside.length())
-			{
-				char c = inside[end];
-				if (stack.size() > 0 && (c == '\"' || c == ']' || c == '}'))
-				{
-					char c_top = stack.front();
-					if (c_top == match_list[c])
-					{
-						stack.pop_front();
-					}
-					continue;
-				}
-				
-				if (c == '\"' || c == '[' || c == '{')
-				{
-					stack.push_front(c);					
-					continue;
-				}
-				
-				if (stack.size()>0 || c != ',')
-				{
-					continue;
-				}
-			}
-
-			std::string sub = inside.substr(start, end - start);			
-			arr->value().emplace_back(Parse(sub.c_str()));
-			start = end + 1;
-			end = start;		
-			
+			return nullptr;
 		}
-		return arr;
-	}
-	
-	if (s[0] == '{')
-	{
-		SPObject obj = JObject::New();
-		std::string inside = s.substr(1, s.length() - 2);
-		std::list<char> stack;
-		int start = 0;
-		int end = start + 1;
-		for (; end <= inside.length(); end++)
+
+		if (s[0] == '\"')
 		{
-			if (end < inside.length())
+			std::string sub = s.substr(1, s.length() - 2);
+			// TODO: need unescape here
+			return String::New(sub.c_str());
+		}
+
+		if (s[0] == '[')
+		{
+			auto arr = Array::New();
+			std::string inside = s.substr(1, s.length() - 2);
+			std::list<char> stack;
+			int start = 0;
+			int end = start;
+
+			for (; end <= inside.length(); end++)
 			{
-				char c = inside[end];
-				if (stack.size() > 0 && (c == '\"' || c == ']' || c == '}'))
+				if (end < inside.length())
 				{
-					char c_top = stack.front();
-					if (c_top == match_list[c])
+					char c = inside[end];
+					if (stack.size() > 0 && (c == '\"' || c == ']' || c == '}'))
 					{
-						stack.pop_front();
+						char c_top = stack.front();
+						if (c == match_list[c_top])
+						{
+							stack.pop_front();
+						}
+						continue;
 					}
-					continue;
+
+					if (c == '\"' || c == '[' || c == '{')
+					{
+						stack.push_front(c);
+						continue;
+					}
+
+					if (stack.size() > 0 || c != ',')
+					{
+						if (c == '\\')
+						{
+							end++;
+						}
+						continue;
+					}
 				}
 
-				if (c == '\"' || c == '[' || c == '{')
-				{
-					stack.push_front(c);
-					continue;
-				}
-
-				if (stack.size() > 0 || c != ',')
-				{
-					continue;
-				}
+				std::string sub = inside.substr(start, end - start);
+				arr->emplace_back(Parse(sub.c_str()));
+				start = end + 1;
+				end = start;
 			}
 
-			std::string sub = inside.substr(start, end - start);			
-			size_t mid = sub.find(':', 0);
-			if (mid == std::string::npos)
+			return arr;
+		}
+
+		if (s[0] == '{')
+		{
+			SPObject obj = Object::New();
+			std::string inside = s.substr(1, s.length() - 2);
+			std::list<char> stack;
+			int start = 0;
+			int end = start + 1;
+			for (; end <= inside.length(); end++)
 			{
-				if (sub.length() > 1 && sub[0] == '\"')
+				if (end < inside.length())
 				{
-					obj->value()[sub.substr(1, sub.length() - 2)] = JNull::New();
+					char c = inside[end];
+					if (stack.size() > 0 && (c == '\"' || c == ']' || c == '}'))
+					{
+						char c_top = stack.front();
+						if (c == match_list[c_top])
+						{
+							stack.pop_front();
+						}
+						continue;
+					}
+
+					if (c == '\"' || c == '[' || c == '{')
+					{
+						stack.push_front(c);
+						continue;
+					}
+
+					if (stack.size() > 0 || c != ',')
+					{
+						if (c == '\\')
+						{
+							end++;
+						}
+						continue;
+					}
+				}
+
+				std::string sub = inside.substr(start, end - start);
+				size_t mid = sub.find(':', 0);
+				if (mid == std::string::npos)
+				{
+					if (sub.length() > 1 && sub[0] == '\"')
+					{
+						(*obj)[sub.substr(1, sub.length() - 2)] = nullptr;
+					}
+					else
+					{
+						(*obj)[sub] = nullptr;
+					}
 				}
 				else
 				{
-					obj->value()[sub] = JNull::New();
+					std::string key_s = sub.substr(0, mid);
+					trim(key_s);
+					std::string value = sub.substr(mid + 1);
+					if (key_s.length() > 1 && key_s[0] == '\"')
+					{
+						(*obj)[key_s.substr(1, key_s.length() - 2)] = Parse(value.c_str());
+					}
+					else
+					{
+						(*obj)[key_s] = Parse(value.c_str());
+					}
 				}
+				start = end + 1;
+				end = start;
 			}
-			else
-			{
-				std::string key_s = sub.substr(0, mid);
-				trim(key_s);
-				std::string value = sub.substr(mid + 1);
-				if (key_s.length() > 1 && key_s[0] == '\"')
-				{
-					obj->value()[key_s.substr(1, key_s.length() - 2)] = Parse(value.c_str());
-				}
-				else
-				{
-					obj->value()[key_s] = Parse(value.c_str());
-				}
-			}			
-			start = end + 1;
-			end = start;
+
+			return obj;
 		}
 
-		return obj;
+		if (s == "null")
+		{
+			return nullptr;
+		}
+		if (s == "true")
+		{
+			return Boolean::New(true);
+		}
 
+		if (s == "false")
+		{
+			return Boolean::New(false);
+		}
+
+		return Number::New(atof(s.c_str()));
 	}
-	
-	if (s == "null")
-	{
-		return JNull::New();
-	}
-	if (s == "true")
-	{
-		return JBoolean::New(true);
-	}
-	
-	if (s == "false")
-	{
-		return JBoolean::New(false);
-	}
-	
-	return JNumber::New(atof(s.c_str()));
 }
+
--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include "json.hpp"
 
+using namespace DataModel;
+using namespace Json;
+
 int main()
 {
-	auto obj = JObject::New();
-	auto str = JString::New("Hello World");
-	obj->value()["title"] = str;
-	auto lst = JArray::New();
-	lst->value() = { JNumber::New(10.0), JNumber::New(20.0), JNumber::New(30.0) };
-	obj->value()["list"] = lst;
+	auto s = String::New("Hello World");
+	auto a = Array::New();
+	*a = { Number::New(10.0), Number::New(20.0), Number::New(30.0) };
 
+	auto obj = Object::New();
+	(*obj)["title"] = s;
+	(*obj)["list"] = a;
 
 	auto jsonstr = Strigify(obj);
 	auto t = Parse(jsonstr.c_str());
 	jsonstr = Strigify(t);
-
+	
 	FILE* fp = fopen("test.json", "w");
 	fprintf(fp, "%s\n", jsonstr.c_str());
 	fclose(fp);
-
-
-	/*auto t = Parse("{\"list\": [\"Hello World\", 1234.5]}");
-	printf("%f\n", t->To<JObject>()->value()["list"]->To<JArray>()->value()[1]->To<JNumber>()->value());*/
 	
 	return 0;
 }
