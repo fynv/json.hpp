9ebb5dbfc908261b2a93e7d452a1a13397ee2722
feiy <feiy@nvidia.com>
2023-12-27 13:52:09 +0800

    unicode test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace Json;
 
 int main()
 {
-	auto s = String::New("Hello\t World\n");
+	auto s = String::New("Hello\t World\n 天地");
 	auto a = Array::New();
 	*a = { Number::New(10.0), Number::New(20.0), Number::New(30.0) };
 
@@ -13,14 +13,14 @@ int main()
 	(*obj)["title"] = s;
 	(*obj)["list"] = a;
 
-	auto jsonstr = Strigify(obj);
+	auto jsonstr = Strigify(obj, true);
 	auto t = Parse(jsonstr.c_str());
-	jsonstr = Strigify(t);
+	jsonstr = Strigify(t, true);
 	
 	FILE* fp = fopen("test.json", "w");
 	fprintf(fp, "%s\n", jsonstr.c_str());
 	fclose(fp);
-	
+
 	return 0;
 }
 
